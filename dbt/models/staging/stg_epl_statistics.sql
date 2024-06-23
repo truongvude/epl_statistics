fdaedc692de541daf8af64acfc7e9ef0b1d1037c
Vu Le Truong <truongvude@outlook.com>
2024-06-23 17:56:34 +0000

    update models and macros

---
+++ dbt/models/staging/stg_epl_statistics.sql
@@ -0,0 +1,36 @@
+{{ config(materialized='view') }}
+
+    select
+        {{ get_date(Date) }} as match_date,
+        cast(Time as TIME FORMAT 'HH24:MI') as match_time,
+        hometeam,
+        awayteam,
+        fthg,
+        ftag,
+        {{ get_result_ftr_description("ftr") }} as FTR,
+        hthg,
+        htag,
+        {{ get_result_htr_description("htr") }} as HTR,
+        referee,
+        hs,
+        epl_external_table.AS,
+        hst,
+        ast,
+        hf,
+        af,
+        hc,
+        ac,
+        hy,
+        ay,
+        hr,
+        ar
+
+    from {{ source('staging', 'epl_external_table') }}
+    where FTR is not Null
+
+-- dbt build --m <model.sql> --var 'is_test_run: false'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
